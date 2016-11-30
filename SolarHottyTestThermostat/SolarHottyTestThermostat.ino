9620eb4404a03526147e8c35afdcc9ad096ab95f
yhendricks <yhendricks1@gmail.com>
2016-11-30 17:16:19 +0200

    Added ability to check for AC current

--- SolarHottyTestThermostat/SolarHottyTestThermostat.ino
+++ SolarHottyTestThermostat/SolarHottyTestThermostat.ino
@@ -1,4 +1,5 @@
 #include <EEPROM.h>
+#include <Filters.h>
 
 
 #define VER_NUM             0.1           // File version number
@@ -11,11 +12,39 @@ float maxAmps = 0;
 float minAmps = 0;
 float lastAmps = 0;
 float noise = 0;
+unsigned long print_DC_Period = 1000; // in milliseconds 
+unsigned long previous_DC_Millis = 0;  // Track time in milliseconds since last reading
+
+// AC current measurement variables
+float testFrequency = 60;                     // test signal frequency (Hz)
+float windowLength = 20.0/testFrequency;     // how long to average the signal, for statistist
+float intercept = -0.1129; // to be adjusted based on calibration testing
+float slope = 0.0405; // to be adjusted based on calibration testing
+float current_amps; // estimated actual current in amps
+unsigned long print_AC_Period = 1000; // in milliseconds 
+unsigned long previous_AC_Millis = 0;  // Track time in milliseconds since last reading
+RunningStatistics inputStats;                 // create statistics to look at the raw test signal
+
+// LED status
+unsigned long led_blink_period = 1000; // in milliseconds 
+unsigned long last_led_check = 0;       // Track time in milliseconds since last reading
+bool led_state = false;
+
+// debug flag
+bool debug_on = false;
 
 enum thermoState {
     OPEN,
     CLOSED
 };
+
+enum HottyState {
+    NOT_KNOWN,
+    AC_DETECTED,
+    DC_DETECTED
+};
+
+enum HottyState hottyState = NOT_KNOWN;
 
 template <class T> int EEPROM_writeAnything(int ee, const T& value)
 {
@@ -26,7 +55,6 @@ template <class T> int EEPROM_writeAnything(int ee, const T& value)
     return i;
 }
 
-
 template <class T> int EEPROM_readAnything(int ee, T& value)
 {
     byte* p = (byte*)(void*)&value;
@@ -36,16 +64,40 @@ template <class T> int EEPROM_readAnything(int ee, T& value)
     return i;
 }
 
-
 struct config_t
 {
     long counter;
 } configuration;
 
 
+bool check_ac_current(int sensorValue) {
+    //sensorValue = analogRead(A0);  // read the analog in value:
+    inputStats.input(sensorValue);  // log to Stats function
+        
+    if((unsigned long)(millis() - previous_AC_Millis) >= print_AC_Period) {
+      previous_AC_Millis = millis();   // update time
+
+      if (debug_on) {
+        // display current values to the screen
+        Serial.print( "\n" );
+        // output sigma or variation values associated with the inputValue itsel
+        Serial.print( "\tsigma: " ); Serial.print( inputStats.sigma() );
+        // convert signal sigma value to current in amps
+        current_amps = intercept + slope * inputStats.sigma();
+        Serial.print( "\tamps: " ); Serial.print( current_amps );
+      }
+      if ((inputStats.sigma() > 50) && (hottyState != AC_DETECTED)) {
+        Serial.println("\nHotty running on AC current ");
+        hottyState = AC_DETECTED;
+        led_blink_period = 1000;
+      }
+    }
+    return (inputStats.sigma() > 50);    
+}
+
 void setup()
 {    
-    Serial.begin(9600);
+    Serial.begin(57600);
     pinMode(WATER_PUMP_PIN, OUTPUT);
     pinMode(LED_BUILTIN, OUTPUT);
     // Read the configuration i.e. the total number of contact breaks of the thermostat
@@ -57,10 +109,10 @@ void setup()
     Serial.println("Press x to reset the counter");   
 }
   
-enum thermoState get_thermostat_state()
+enum thermoState get_thermostat_state(int raw_value)
 {
 //    return digitalRead(MEASURE_THERMO_CURRENT_PIN) ?  OPEN :  CLOSED;
-    return measure_current() > 0.5 ? CLOSED : OPEN;
+    return measure_dc_current(raw_value) > 0.5 ? CLOSED : OPEN;
 }
 
 void print_configuration()
@@ -80,66 +132,97 @@ int ACSoffset = 2500;
 double Voltage = 0;
 double Amps = 0;
 
-float measure_current() {
-    RawValue = analogRead(A0);
-    Serial.print(RawValue);
-    Serial.print(" ");
-    Voltage = (RawValue / 1024.0) * 5000; // Gets you mV
-    amps = ((Voltage - ACSoffset) / mVperAmp); 
-    
-    //amps = (516 - RawValue) * 27.03 / 1023;
-    amps = (amps + lastAmps) / 2;
-    lastAmps = amps;
-    maxAmps = max(maxAmps, amps);
-    minAmps = min(minAmps, amps);
-    noise = maxAmps - minAmps;
-    Serial.print(amps);
-    Serial.print(" ");
-    Serial.println(noise);
-    if (Serial.read() != -1) 
-        {maxAmps = amps; minAmps = amps;}
+float measure_dc_current(int RawValue) {       
+
+      Voltage = (RawValue / 1024.0) * 5000; // Gets you mV
+      amps = ((Voltage - ACSoffset) / mVperAmp); 
+      
+      //amps = (516 - RawValue) * 27.03 / 1023;
+      amps = (amps + lastAmps) / 2;
+      lastAmps = amps;
+      maxAmps = max(maxAmps, amps);
+      minAmps = min(minAmps, amps);
+      noise = maxAmps - minAmps;
+      if (Serial.read() != -1) 
+          {maxAmps = amps; minAmps = amps;}
+    if((unsigned long)(millis() - previous_DC_Millis) >= print_DC_Period) {
+      previous_DC_Millis = millis();   // update time
+      if (debug_on) {
+        Serial.print( "\n" );
+        Serial.print("raw value: "); 
+        Serial.print(RawValue);
+        Serial.print(" ");
+        Serial.print(", amps: ");
+        Serial.print(amps);
+        Serial.print(" ");
+        Serial.print(", noise: "); 
+        Serial.print(noise);
+      }
+      if (hottyState != DC_DETECTED) {
+        hottyState = DC_DETECTED;
+        led_blink_period = 400;
+        Serial.println("\nHotty running on DC current ");
+      }
+    }
     return amps;
 }
 
 void loop() {
     byte byteRead;
+    inputStats.setWindowSecs( windowLength );
     while (1) {
-        //measure_current();
-        if (get_thermostat_state() == CLOSED) {
-            // There is DC current flowing
-            if (thermostat_state == OPEN) {
-                configuration.counter++;
-                thermostat_state = CLOSED;
-                digitalWrite(WATER_PUMP_PIN, HIGH);              // switch OFF water pump
-                print_thermostat_state();
-                print_configuration();
-            }
+        RawValue = analogRead(A0);
+        if (check_ac_current(RawValue) == false) 
+        {          
+          if (get_thermostat_state(RawValue) == CLOSED) {
+              // There is DC current flowing
+              if (thermostat_state == OPEN) {
+                  configuration.counter++;
+                  thermostat_state = CLOSED;
+                  digitalWrite(WATER_PUMP_PIN, HIGH);              // switch OFF water pump
+                  print_thermostat_state();
+                  print_configuration();
+              }
+          }
+          else {
+              // There is NO DC current flowing
+              if (thermostat_state == CLOSED)
+              {
+                  thermostat_state = OPEN;   
+                  print_thermostat_state();    
+                  digitalWrite(WATER_PUMP_PIN, LOW);             // switch ON water pump
+              }        
+          }
         }
-        else {
-            // There is NO DC current flowing
-            if (thermostat_state == CLOSED)
-            {
-                thermostat_state = OPEN;   
-                print_thermostat_state();    
-                digitalWrite(WATER_PUMP_PIN, LOW);             // switch ON water pump
-            }        
+        else{
+          digitalWrite(WATER_PUMP_PIN, LOW);             // switch ON water pump          
         }
-        /*  check if data has been sent from the computer: */
+        //  check if data has been sent from the computer: 
         if (Serial.available()) {
-            /* read the most recent byte */
+            // read the most recent byte 
             byteRead = Serial.read();
             if (byteRead == 'x') {
                 // Reset the counter
                 configuration.counter = 0;
                 print_configuration();
+            } 
+            else if (byteRead == 'd') {
+              debug_on = !debug_on;            
             }
         }
 
         EEPROM_writeAnything(81, configuration);
-        digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-        delay(500);                       
-        digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-        delay(500);
+        if((unsigned long)(millis() - last_led_check) >= (led_blink_period /2)) {
+          last_led_check = millis();   // update time
+          if (led_state) {
+            led_state = false;
+            digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+          }
+          else {          
+            led_state = true;
+            digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+          }
+        }       
     }
 }
 
